3303810145b7ed40301c2821d7512dc2015a088a
afifuddin <afifbasya@gmail.com>
2020-07-25 21:15:06 +0700

    5. Membuat Header Component

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 import Logo from './logo.png'
 import SplashBackground from './SplashBackground.png'
+import ImageHeader from './header.png'
 
-export { Logo, SplashBackground }
+export { Logo, SplashBackground, ImageHeader }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,56 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
+import {ImageHeader, Logo} from '../../assets';
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Beranda</Text>
+  return (
+    <View style={styles.page}>
+      <ImageBackground source={ImageHeader} style={styles.header}>
+        <Image source={Logo} style={styles.logo} />
+        <View style={styles.hello}>
+          <Text style={styles.selamat}>Selamat Datang, </Text>
+          <Text style={styles.username}>afifbasya</Text>
         </View>
-    )
-}
+      </ImageBackground>
+    </View>
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  header: {
+    width: windowWidth,
+    height: windowHeight * 0.3,
+    paddingHorizontal: 30,
+    paddingTop: 10,
+  },
+  logo: {
+    width: windowWidth * 0.25,
+    height: windowHeight * 0.06,
+  },
+  hello: {
+      marginTop: windowHeight*0.025
+  },
+  selamat : {
+      fontSize: 24,
+      fontFamily: 'TitilliumWeb-Regular'
+  },
+  username: {
+      fontSize: 22,
+      fontFamily: 'TitilliumWeb-Bold'
+  }
+});
